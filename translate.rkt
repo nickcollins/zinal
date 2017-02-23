ac52db22a27f3837e508f359d598a1e7ec84de16
Nick Collins <nick.m.collins@gmail.com>
2017-02-23 01:02:53 -0800

    Add oop stuff to translate.rkt
    translation is very basic and doesn't bother getting super classes right, or
    the methods of method invokations. this stuff must be polished up and augmented
    later. Also, the translation is barely tested, since it's hard to test anything
    without gui support. Also, translation of is-a? isn't done, since is-a? is
    about to be removed from the backend

--- translate.rkt
+++ translate.rkt
@@ -29,53 +29,56 @@
   (void)
 )
 
+(define (translate-list-like inserter list-data)
+  (map-by-index
+    (lambda (i v) (translate-datum v (inserter i)))
+    list-data
+  )
+)
+
 (define (translate-list list-data db-list)
-  (define vec (list->vector list-data))
-  (build-list
-    (vector-length vec)
-    (lambda (i)
-      (translate-datum (vector-ref vec i) (send db-list insert!! i))
+  (translate-list-like (lambda (i) (send db-list insert!! i)) list-data)
+)
+
+(define (translate-args args-data db-has-args)
+  (translate-list-like (lambda (i) (send db-has-args insert-arg!! i)) args-data)
+)
+
+(define (translate-body body-data db-has-body)
+  (translate-list-like (lambda (i) (send db-has-body insert-into-body!! i)) body-data)
+)
+
+(define (translate-params params-data db-has-params)
+
+  (define required-params (takef params-data (negate list?)))
+  (map-by-index
+    (lambda (i param)
+      (assert (format "required param must be a symbol: ~a" param) (symbol? param))
+      (send db-has-params insert-required-param!! i (symbol->string param))
     )
+    required-params
+  )
+
+  (define optional-params (dropf params-data (negate list?)))
+  (map-by-index
+    (lambda (i param)
+      (assert (format "optional param must be of form [name value]: ~a" param) (and (list? param) (= 2 (length param))))
+      (define name (first param))
+      (define value (second param))
+      (assert (format "name must be a symbol: ~a" name) (symbol? name))
+      (define db-param (send db-has-params insert-optional-param!! i (symbol->string name)))
+      (translate-datum value (send db-param get-default))
+    )
+    optional-params
   )
 )
 
 (define (translate-lambda params body db-unassigned)
   (assert (format "lambda params must be a list: ~a" params) (list? params))
   (assert (format "lambda must have non-empty body: ~a" body) (pair? body))
-
   (define db-lambda (send db-unassigned assign-lambda!!))
-
-  (define required-params (list->vector (takef params (negate list?))))
-  (build-list
-    (vector-length required-params)
-    (lambda (i)
-      (define param (vector-ref required-params i))
-      (assert (format "required param must be a symbol: ~a" param) (symbol? param))
-      (send db-lambda insert-required-param!! i (symbol->string param))
-    )
-  )
-
-  (define optional-params (list->vector (dropf params (negate list?))))
-  (build-list
-    (vector-length optional-params)
-    (lambda (i)
-      (define param (vector-ref optional-params i))
-      (assert (format "optional param must be of form [name value]: ~a" param) (and (list? param) (= 2 (length param))))
-      (define name (first param))
-      (define value (second param))
-      (assert (format "name must be a symbol: ~a" name) (symbol? name))
-      (define db-param (send db-lambda insert-optional-param!! i (symbol->string name)))
-      (translate-datum value (send db-param get-default))
-    )
-  )
-
-  (define body-vec (list->vector body))
-  (build-list
-    (vector-length body-vec)
-    (lambda (i)
-      (translate-datum (vector-ref body-vec i) (send db-lambda insert-into-body!! i))
-    )
-  )
+  (translate-params params db-lambda)
+  (translate-body body db-lambda)
 )
 
 (define (translate-assert assertion format-string format-args db-unassigned)
@@ -84,12 +87,119 @@
   (translate-datum assertion (send db-assert get-assertion))
   (translate-datum format-string (send db-assert get-format-string))
 
-  (define format-args-vec (list->vector format-args))
-  (build-list
-    (vector-length format-args-vec)
-    (lambda (i)
-      (translate-datum (vector-ref format-args-vec i) (send db-assert insert-format-arg!! i))
+  (map-by-index
+    (lambda (i v) (translate-datum v (send db-assert insert-format-arg!! i)))
+    format-args
+  )
+)
+
+(define (translate-class class-data db-unassigned [definition-name #f])
+  (define class-symbol (first class-data))
+  (define (super*) (second class-data))
+  (assert
+    (format "class definition must be of form (class*\\? super (interfaces ...)\\? (init params ...)\\? (abstract m)... body ...): ~a" class-data)
+    (and (> (length class-data) 2) (symbol? (super*)))
+  )
+  (define db-class
+    (if definition-name
+      (send db-unassigned assign-define-class!! definition-name)
+      (send db-unassigned assign-class-instance!!)
     )
+  )
+  (send db-class set-legacy-super-class!! #f (~a (super*)))
+  (define body (drop class-data 2))
+  (when (equal? class-symbol 'class*)
+    (define interfaces (car body))
+    (assert
+      (format "class* definition must have interfaces list: ~a" class-data)
+      (and (list? interfaces) (andmap symbol? interfaces) (> (length body) 1))
+    )
+    (add-direct-super-interfaces!! (send db-class get-db) interfaces db-class)
+    (set! body (cdr body))
+  )
+  (define possible-init (first body))
+  (when (and (pair? possible-init) (equal? 'init (first possible-init)))
+    (assert (format "class-instance can't have init params: ~a" class-data) definition-name)
+    (assert (format "class*\\? definition with init has no body: ~a" class-data) (> (length body) 1))
+    (translate-params (cdr possible-init) db-class)
+    (set! body (cdr body))
+  )
+  (define abstract? (conjoin pair? (compose1 (curry equal? 'abstract) first)))
+  (define abstracts (takef body abstract?))
+  (for-each
+    (lambda (abstract)
+      (assert (format "class-instance can't have abstract methods: ~a" class-data) definition-name)
+      (assert (format "invalid abstract clause: ~a" abstract) (and (= 2 (length abstract)) (symbol? (second abstract))))
+      (send db-class add-direct-method!! (~a (second abstract)))
+    )
+    abstracts
+  )
+  (translate-body (dropf body abstract?) db-class)
+)
+
+(define (translate-define/override-method define-method-data db-unassigned)
+  (define (name+params) (second define-method-data))
+  (define (name-symbol) (car (name+params)))
+  (define (params) (cdr (name+params)))
+  (assert
+    (format "invalid method definition/override: ~a" define-method-data)
+    (and (> (length define-method-data) 2) (pair? (name+params)) (symbol? (name-symbol)))
+  )
+  (define containing-class (get-class-parent db-unassigned))
+  (define name (~a (name-symbol)))
+  (define existing-method (findf (lambda (m) (equal? name (send m get-short-desc))) (send containing-class get-all-methods)))
+  (define db-define/override-method-node
+    (if existing-method
+      (send db-unassigned assign-define-method!! existing-method)
+      (if (equal? (first define-method-data) 'define/public)
+        (send db-unassigned assign-define-method!! (send containing-class add-direct-method!! name))
+        (send db-unassigned assign-override-legacy-method!! name)
+      )
+    )
+  )
+  (define db-lambda (send db-define/override-method-node get-lambda))
+  (translate-params (params) db-lambda)
+  (translate-body (drop define-method-data 2) db-lambda)
+)
+
+(define (translate-define datum db-unassigned)
+  (define second* (second datum))
+  (cond
+    [(pair? second*)
+      (define name (car second*))
+      (define params (cdr second*))
+      (define body (drop datum 2))
+      (translate-lambda params body (send (send db-unassigned assign-def!! (symbol->string name)) get-expr))
+    ]
+    [(symbol? second*)
+      (assert (format "non-function define must be of form (define a b): ~a" datum) (= 3 (length datum)))
+      (define db (send db-unassigned get-db))
+      (define defined-name (symbol->string second*))
+      (define defined (third datum))
+      (define (defined-is? expected-first) (and (pair? defined) (equal? expected-first (first defined))))
+      (cond
+        [(defined-is? 'interface)
+          (define (supers) (second defined))
+          (define (methods) (drop defined 2))
+          (assert
+            (format "interface definition must be of form (interface (supers ...) methods ...): ~a" defined)
+            (and (>= (length defined) 2) (list? (supers)) (andmap symbol? (supers)) (andmap symbol? (methods)))
+          )
+          (define db-iface (send db create-interface!! defined-name))
+          (add-direct-super-interfaces!! db supers db-iface)
+          (for-each (lambda (m) (send db-iface add-direct-method!! m)) (methods))
+        ]
+        [(ormap defined-is? '(class class*))
+          (translate-class defined db-unassigned defined-name)
+        ]
+        [else
+          (translate-datum (third datum) (send (send db-unassigned assign-def!! defined-name) get-expr))
+        ]
+      )
+    ]
+    [else
+      (error 'translate-define "the second part of a define must be a list or symbol: ~a" datum)
+    ]
   )
 )
 
@@ -110,22 +220,7 @@
           (translate-lambda params body db-unassigned)
         ]
         [(equal? 'define (first-item))
-          (define second* (second datum))
-          (cond
-            [(pair? second*)
-              (define name (car second*))
-              (define params (cdr second*))
-              (define body (drop datum 2))
-              (translate-lambda params body (send (send db-unassigned assign-def!! (symbol->string name)) get-expr))
-            ]
-            [(symbol? second*)
-              (assert (format "non-function define must be of form (define a b): ~a" datum) (= 3 (length datum)))
-              (translate-datum (third datum) (send (send db-unassigned assign-def!! (symbol->string second*)) get-expr))
-            ]
-            [else
-              (error 'translate-datum "the second part of a define must be a list or symbol: ~a" datum)
-            ]
-          )
+          (translate-define datum db-unassigned)
         ]
         [(equal? 'assert (first-item))
           (define msg (second datum))
@@ -135,10 +230,50 @@
             (translate-assert assertion msg '() db-unassigned)
           )
         ]
+        [(equal? 'make-object (first-item))
+          (define class* (second datum))
+          (cond
+            [(list? class*)
+              (define class-symbol (first class*))
+              (assert (format "invalid make-object: ~a" datum) (member class-symbol '(class class*)))
+              (translate-class class* db-unassigned)
+            ]
+            [(symbol? class*)
+              (define db-create-object (send db-unassigned assign-create-object!!))
+              (send (send db-create-object get-class-node) set-short-desc!! (~a class*))
+              (translate-args (drop datum 2) db-create-object)
+            ]
+            [else
+              (error 'translate-datum "invalid make-object: ~a" datum)
+            ]
+          )
+        ]
+        [(member (first-item) '(define/public define/override))
+          (translate-define/override-method datum db-unassigned)
+        ]
+        [(equal? 'send (first-item))
+          (define (method-symbol) (third datum))
+          (assert (format "invalid method invokation: ~a" datum) (and (> (length datum) 2) (symbol? (method-symbol))))
+          (define db-invoke (send db-unassigned assign-invoke-legacy-method!! (~a (method-symbol))))
+          (translate-datum (second datum) (send db-invoke get-object))
+          (translate-args (drop datum 3) db-invoke)
+        ]
+        [(equal? 'super-make-object (first-item))
+          (translate-args (cdr datum) (send db-unassigned assign-super-init!!))
+        ]
+        [(equal? 'super (first-item))
+          (define (method-symbol) (second datum))
+          (assert (format "invalid super invokation: ~a" datum) (and (> (length datum) 1) (symbol? (method-symbol))))
+          (define db-invoke (send db-unassigned assign-invoke-legacy-super-method!! (~a (method-symbol))))
+          (translate-args (drop datum 2) db-invoke)
+        ]
         [else
           (translate-list datum (send db-unassigned assign-list!!))
         ]
       )
+    ]
+    [(equal? 'this datum)
+      (send db-unassigned assign-this!!)
     ]
     [(number? datum)
       (send db-unassigned assign-number!! datum)
@@ -165,8 +300,30 @@
   )
 )
 
+(define (add-direct-super-interfaces!! db super-interface-symbols db-sub-type)
+  (define all-interfaces (send db get-all-interfaces))
+  (for-each
+    (lambda (s)
+      (define super-name (symbol->string s))
+      (define db-super (findf (lambda (i) (equal? super-name (send i get-short-desc))) all-interfaces))
+      (when db-super
+        (send db-sub-type add-direct-super-interface!! db-super)
+      )
+    )
+    super-interface-symbols
+  )
+)
+
+(define (get-class-parent db-node)
+  (assert "no class parent found" db-node)
+  (if (is-a? db-node zinal:db:class%%)
+    db-node
+    (get-class-parent (send db-node get-parent))
+  )
+)
+
 ; Example use:
 ; (require "sql-db.rkt")
 ; (define main-db (make-object zinal:sql-db% "junk.db"))
-; (translate "translate.rkt" main-db)
+; (translate "sql-db.rkt" main-db)
 )
